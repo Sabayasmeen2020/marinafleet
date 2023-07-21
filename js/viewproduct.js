1e25b60b4e221af4ad08f494d3c51267480bb195
mehreen-faiz <139073527+mehreen-faiz@users.noreply.github.com>
2023-07-20 20:35:07 -0700

    changes

--- js/viewproduct.js
+++ js/viewproduct.js
@@ -7,6 +7,22 @@ $.get("js/data.json", function(response){
             $("#details").text(i.details);
             $("#name").text(i.name);
             $("#price").text(i.price);
+            $("#rent").text(i.rent);
+
+        }
+    }
+})
+
+var id = sessionStorage.getItem('productId');
+console.log(id);
+$.get("js/data1.json", function(response){
+    for(i of response){
+        if(i.id == id){
+            $("#image").attr("src","images/"+i.image);
+            $("#details").text(i.details);
+            $("#name").text(i.name);
+            $("#price").text(i.price);
+            $("#rent").text(i.rent);
 
         }
     }
