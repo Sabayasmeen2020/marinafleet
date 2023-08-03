ebffdfb9c9dc33b7d79e5a13b96a2cc626abb80b
mehreen-faiz <139073527+mehreen-faiz@users.noreply.github.com>
2023-08-03 15:32:39 -0700

    styling

--- js/viewproduct.js
+++ js/viewproduct.js
@@ -1,3 +1,4 @@
+
 var id = sessionStorage.getItem('productId');
 console.log(id);
 $.get("js/data.json", function(response){
@@ -7,24 +8,7 @@ $.get("js/data.json", function(response){
             $("#details").text(i.details);
             $("#name").text(i.name);
             $("#price").text(i.price);
-            $("#rent").text(i.rent);
 
         }
     }
 })
-
-var id = sessionStorage.getItem('productId');
-console.log(id);
-$.get("js/data1.json", function(response){
-    for(i of response){
-        if(i.id == id){
-            $("#image").attr("src","images/"+i.image);
-            $("#details").text(i.details);
-            $("#name").text(i.name);
-            $("#price").text(i.price);
-            $("#rent").text(i.rent);
-
-        }
-    }
-})
-
