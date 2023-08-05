08bcd9e426a0cc1920464c9ede57c1f8cb20ea28
Misbah-hanif <138647889+Misbah-hanif@users.noreply.github.com>
2023-08-05 04:15:01 -0700

    DONE

--- js/militrayquery.js
+++ js/militrayquery.js
@@ -1,33 +1,7 @@
 $(document).ready(function(){
-    $.get("js/militraydata.json",function(response){
-        for ( i of response) {
-            $("#cardBox").append(`
-            <div class="col-md-4" >
-            <br><br>
-            <div class="flip-card">
-
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="images/${i.image} "style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back">
-
-      <h5 class="card-title" >${i.name}</h5>
-        <p class="card-text1">${i.price}</p>
-          <p class="card-text">${i.details}</p>
-          <button class="btm" onclick="viewproduct(${i.id})">DETAILS</button> </div>
-    </div>
-  </div>
-</div>
-            </div>
-        `);
-        }
-    })
- })
- $(document).ready(function(){
-  $.get("js/data1.json",function(response){
+  $.get("js/militraydata.json",function(response){
       for ( i of response) {
-          $("#cardBox1").append(`
+          $("#cardBox").append(`
           <div class="col-md-4" >
           <br><br>
           <div class="flip-card">
@@ -41,7 +15,7 @@ $(document).ready(function(){
     <h5 class="card-title" >${i.name}</h5>
       <p class="card-text1">${i.price}</p>
         <p class="card-text">${i.details}</p>
-        <button class="btm" onclick="viewproduct(${i.id})">DETAILS</button> </div>
+        <button class="btm" onclick="militrayviewproduct(${i.id})">DETAILS</button> </div>
   </div>
 </div>
 </div>
@@ -50,8 +24,34 @@ $(document).ready(function(){
       }
   })
 })
+$(document).ready(function(){
+$.get("js/data1.json",function(response){
+    for ( i of response) {
+        $("#cardBox1").append(`
+        <div class="col-md-4" >
+        <br><br>
+        <div class="flip-card">
+
+<div class="flip-card-inner">
+<div class="flip-card-front">
+  <img src="images/${i.image} "style="width:300px;height:300px;">
+</div>
+<div class="flip-card-back">
+
+  <h5 class="card-title" >${i.name}</h5>
+    <p class="card-text1">${i.price}</p>
+      <p class="card-text">${i.details}</p>
+      <button class="btm" onclick="viewproduct(${i.id})">DETAILS</button> </div>
+</div>
+</div>
+</div>
+        </div>
+    `);
+    }
+})
+})
 
 
- function viewproduct(id){
-    sessionStorage.setItem('productId',id);
-    location.href = "militrayviewproduct.html";}
+function viewproduct(id){
+  sessionStorage.setItem('productId',id);
+  location.href = "militrayviewproduct.html";}
--- js/militrayviewproduct.js
+++ js/militrayviewproduct.js
@@ -1,30 +1,14 @@
+
 var id = sessionStorage.getItem('productId');
 console.log(id);
-$.get("js/militarydata.json", function(response){
+$.get("js/militraydata.json", function(response){
     for(i of response){
         if(i.id == id){
             $("#image").attr("src","images/"+i.image);
             $("#details").text(i.details);
             $("#name").text(i.name);
             $("#price").text(i.price);
-            $("#rent").text(i.rent);
-
+            $("#pdf").attr('href',i.pdf);
         }
     }
 })
-
-var id = sessionStorage.getItem('productId');
-console.log(id);
-$.get("js/militraydata1.jon", function(response){
-    for(i of response){
-        if(i.id == id){
-            $("#image").attr("src","images/"+i.image);
-            $("#details").text(i.details);
-            $("#name").text(i.name);
-            $("#price").text(i.price);
-            $("#rent").text(i.rent);
-
-        }
-    }
-})
-g
