6e30aceaffb0f213881e0220775d36f5e3689116
zainabqadri <138647800+zainabqadri@users.noreply.github.com>
2023-08-06 22:07:30 +0500

    zainab

---
+++ js/sailingguery.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $.get("js/sailing.json",function(response){
+        for ( i of response) {
+            $("#cardBox").append(`
+            <div class="col-md-4" >
+            <br><br>
+            <div class="flip-card">
+
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="images/${i.image} "style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+
+      <h5 class="card-title" >${i.name}</h5>
+        <p class="card-text1">${i.price}</p>
+          <p class="card-text">${i.details}</p>
+          <button class="btm" onclick="sailingviewproduct(${i.id})">DETAILS</button> </div>
+    </div>
+  </div>
+</div>
+            </div>
+        `);
+        }
+    })
+ })
+ $(document).ready(function(){
+  $.get("js/sailing.json",function(response){
+      for ( i of response) {
+          $("#cardBox1").append(`
+          <div class="col-md-4" >
+          <br><br>
+          <div class="flip-card">
+
+<div class="flip-card-inner">
+  <div class="flip-card-front">
+    <img src="images/${i.image} "style="width:300px;height:300px;">
+  </div>
+  <div class="flip-card-back">
+
+    <h5 class="card-title" >${i.name}</h5>
+      <p class="card-text1">${i.price}</p>
+        <p class="card-text">${i.details}</p>
+        <button class="btm" onclick="sailingviewproduct(${i.id})">DETAILS</button> </div>
+  </div>
+</div>
+</div>
+          </div>
+      `);
+      }
+  })
+})
+
+
+ function sailingviewproduct(id){
+    sessionStorage.setItem('productId',id);
+    location.href = "sailingviewproduct.html";}
